187a7985ec55d69ab809e41fffffe28b0f22c2fd
kkufieta <katharina.kufieta@gmail.com>
2020-08-09 21:06:54 -0400

    Move placeholder data from BusinessList to App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,12 +3,54 @@ import BusinessList from '../BusinessList/BusinessList.jsx';
 import SearchBar from '../SearchBar/SearchBar.jsx';
 import './App.css';
 
+const businesses = [
+  {
+    id: 1,
+    imageSrc:
+      'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
+  },
+  {
+    id: 2,
+    imageSrc:
+      'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
+  },
+  {
+    id: 3,
+    imageSrc:
+      'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
+  }
+]
+
 function App() {
   return (
     <div className="App">
       <h1>ravenous</h1>
       <SearchBar />
-      <BusinessList />
+      <BusinessList businesses={businesses} />
     </div>
   );
 }
--- src/components/BusinessList/BusinessList.jsx
+++ src/components/BusinessList/BusinessList.jsx
@@ -2,50 +2,10 @@ import React from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business.jsx';
 
-const businesses = [
-    {
-        id: 1,
-        imageSrc:
-            'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-        name: 'MarginOtto Pizzeria',
-        address: '1010 Paddington Way',
-        city: 'Flavortown',
-        state: 'NY',
-        zipCode: '10101',
-        category: 'Italian',
-        rating: 4.5,
-        reviewCount: 90
-    },
-    {
-        id: 2,
-        imageSrc:
-            'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-        name: 'MarginOtto Pizzeria',
-        address: '1010 Paddington Way',
-        city: 'Flavortown',
-        state: 'NY',
-        zipCode: '10101',
-        category: 'Italian',
-        rating: 4.5,
-        reviewCount: 90
-    },
-    {
-        id: 3,
-        imageSrc:
-            'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-        name: 'MarginOtto Pizzeria',
-        address: '1010 Paddington Way',
-        city: 'Flavortown',
-        state: 'NY',
-        zipCode: '10101',
-        category: 'Italian',
-        rating: 4.5,
-        reviewCount: 90
-    }
-]
 
 class BusinessList extends React.Component {
     render() {
+        const businesses = this.props.businesses;
         const businessList = businesses.map((business) => {
             return (
                 <Business
