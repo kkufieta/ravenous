78c3e23a54ee6b8ad793b3d2c0a5a15b5ae39526
kkufieta <katharina.kufieta@gmail.com>
2020-08-09 20:46:28 -0400

    Add the Searchbar component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import BusinessList from '../BusinessList/BusinessList.jsx';
+import SearchBar from '../SearchBar/SearchBar.jsx';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>ravenous</h1>
+      <SearchBar />
       <BusinessList />
     </div>
   );
