4ff09299a30056e517dcc080643e1e7487ba0d74
kkufieta <katharina.kufieta@gmail.com>
2020-08-09 22:09:45 -0400

    Add handlers for input field and button

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -16,10 +16,26 @@ class SearchBar extends React.Component {
             location: '',
             sortBy: 'best_match'
         }
+
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleSortByChange(sortByOption) {
         this.setState({ sortBy: sortByOption });
+    }
+
+    handleTermChange(event) {
+        this.setState({ term: event.target.value });
+    }
+
+    handleLocationChange(event) {
+        this.setState({ location: event.target.value });
+    }
+
+    handleSubmit(event) {
+        console.log("Searching for: ", this.state.term, " in : ", this.state.location, " sorted by: ", this.state.sortBy);
     }
 
     getSortByClass(sortByOption) {
@@ -48,10 +64,14 @@ class SearchBar extends React.Component {
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
-                    <input placeholder="Search Businesses" />
-                    <input placeholder="Where?" />
+                    <input
+                        placeholder="Search Businesses"
+                        onChange={this.handleTermChange} />
+                    <input
+                        placeholder="Where?"
+                        onChange={this.handleLocationChange} />
                 </div>
-                <div className="SearchBar-submit">
+                <div className="SearchBar-submit" onClick={this.handleSubmit}>
                     <a>Let's Go</a>
                 </div>
             </div>
