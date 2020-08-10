f792073ecd26a005fe6d6e0c1ef9c2d3269045dd
kkufieta <katharina.kufieta@gmail.com>
2020-08-10 09:30:30 -0400

    Move yelp result 'businesses' to App state

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,53 +4,20 @@ import SearchBar from '../SearchBar/SearchBar.jsx';
 import Yelp from '../../util/Yelp.jsx';
 import './App.css';
 
-const businesses = [
-  {
-    id: 1,
-    imageSrc:
-      'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-  },
-  {
-    id: 2,
-    imageSrc:
-      'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-  },
-  {
-    id: 3,
-    imageSrc:
-      'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-  }
-]
-
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      businesses: []
+    };
+
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
   async searchYelp(term, location, sortBy) {
-    console.log("searching for ", term, " in location ", location, " sorted by: ", sortBy);
     const businesses = await Yelp.search(term, location, sortBy);
-    console.log(businesses);
+    this.setState({ businesses: businesses });
   }
 
   render() {
@@ -58,7 +25,7 @@ class App extends React.Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={businesses} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
