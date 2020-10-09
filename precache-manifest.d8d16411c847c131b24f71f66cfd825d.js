04b1e3e5cf3c67b676780950abf2ac8a4ddcf27d
kkufieta <katharina.kufieta@gmail.com>
2020-10-09 16:45:20 -0400

    Updates

---
+++ precache-manifest.d8d16411c847c131b24f71f66cfd825d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e27b641d06db295c6480c009c3b679",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "9a20c12ab8e0c889b1f8",
+    "url": "/ravenous/static/css/main.faca228b.chunk.css"
+  },
+  {
+    "revision": "4c71ba671ebd440f47aa",
+    "url": "/ravenous/static/js/2.16dbc130.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ravenous/static/js/2.16dbc130.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a20c12ab8e0c889b1f8",
+    "url": "/ravenous/static/js/main.9fc6ad2f.chunk.js"
+  },
+  {
+    "revision": "9e12bad58d82799d27a7",
+    "url": "/ravenous/static/js/runtime-main.b1d203fc.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
